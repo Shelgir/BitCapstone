ec33216614d5834c262af48806a11dcdf9e46bee
Shelgir <shelgirr@gmail.com>
2021-11-23 22:37:27 +0300

    responsive navbar zammman daraja yak

--- src/components/TheNavbar.jsx
+++ src/components/TheNavbar.jsx
@@ -2,14 +2,54 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useGetCategoriesQuery } from "../services/service-api";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function TheNavbar() {
   const location = useLocation();
   const cart = useSelector((state) => state.cart.value);
   const { data = [] } = useGetCategoriesQuery();
+  const [showMenu, setShowMenu] = useState(false);
 
   const { pathname } = location;
   const splitLocation = pathname.split("/");
+  let menu;
+  if (showMenu) {
+    menu = (
+      <div className="flex flex-col justify-center items-center">
+        <Link
+          to="/cart"
+          className="mx-4 w-full py-2 text-purple-600 bg-white text-center text-xl hover:bg-purple-200"
+        >
+          Cart [ {cart.length} ]
+        </Link>
+        {localStorage.getItem("authToken") ? (
+          <Link
+            to="/profile"
+            className="mx-4 w-full py-2 text-purple-600 bg-white text-center text-xl hover:bg-purple-200"
+          >
+            Profile
+          </Link>
+        ) : (
+          <>
+            <Link
+              to="/register"
+              className="mx-4 w-full py-2 text-purple-600 bg-white text-center text-xl hover:bg-purple-200"
+            >
+              Register
+            </Link>
+            <Link
+              to="/login"
+              className="mx-4 w-full py-2 text-purple-600 bg-white text-center text-xl hover:bg-purple-200"
+            >
+              Login
+            </Link>
+          </>
+        )}
+      </div>
+    );
+  }
   return (
     <div>
       <>
@@ -56,7 +96,20 @@ export default function TheNavbar() {
               </div>
             </div>
           </div>
-          <div>
+          <button onClick={() => setShowMenu(!showMenu)} className="mx-3">
+            {showMenu ? (
+              <FontAwesomeIcon
+                icon={faTimes}
+                className="text-purple-600 hover:text-purple-900 transition-colors transform ease-in md:hidden"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                className="text-purple-600 hover:text-purple-900 transition-colors transform ease-in md:hidden"
+              />
+            )}
+          </button>
+          <div className="hidden md:block ">
             <Link
               to="/cart"
               className={
@@ -104,7 +157,8 @@ export default function TheNavbar() {
               </>
             )}
           </div>
-        </div>
+        </div>{" "}
+        {menu}
       </>
     </div>
   );
