aab781bcc49af34137c5beba60ef97d3d3f06049
Shelgir <shelgirr@gmail.com>
2021-12-14 14:46:01 +0300

    hero section renewed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import jwt_decode from "jwt-decode";
 import { Link } from "react-router-dom";
+import HeroImg from "../images/HeroSectionImg.png";
 
 export default function HeroSection() {
   let fname = "";
@@ -10,20 +11,28 @@ export default function HeroSection() {
     console.log(fname);
   }
   return (
-    <div className="flex justify-center items-center h-screen">
-      {fname ? (
-        <h2 className="text-purple-400 text-6xl font-bold">Welcome {fname}</h2>
-      ) : (
-        <>
-          <h2 className="text-purple-400 mx-2 text-6xl font-bold">Welcome</h2>
-          <Link
-            to="/register"
-            className="py-3 px-2 mx-2 text-purple-400 text-6xl font-bold hover:text-purple-700 transition-all transform ease-in"
-          >
-            Register Here
-          </Link>
-        </>
-      )}
+    <div className="grid grid-cols-3 h-screen">
+      <div className="lg:col-span-1 col-span-3 flex justify-center items-center">
+        {fname ? (
+          <h2 className="text-purple-400 text-6xl font-bold">
+            Welcome {fname}
+          </h2>
+        ) : (
+          <>
+            <h2 className="text-purple-400 mx-2 text-6xl font-bold">Welcome</h2>
+            <Link
+              to="/register"
+              className="pt-3 px-2 mx-2 text-purple-400 text-3xl flex items-end justify-end font-bold hover:text-purple-700 transition-all transform ease-in"
+            >
+              Register Here
+            </Link>
+          </>
+        )}
+      </div>
+      <div className="hidden lg:block"></div>
+      <div className="lg:flex justify-center items-center hidden">
+        <img className="" src={HeroImg} alt="" />
+      </div>
     </div>
   );
 }
